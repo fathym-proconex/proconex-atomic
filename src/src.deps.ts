9a9aa1714b098e4a254e5d668f6ca8ad516d94fd
mcgear <michael.gearhardt@gmail.com>
2024-08-14 11:13:44 -0600

    ref updates

--- src/src.deps.ts
+++ src/src.deps.ts
@@ -1,3 +1,3 @@
-export * from 'jsr:@fathym/common@0';
+export * from 'jsr:@fathym/common@0.0.211';
 
-export * from 'jsr:@fathym/atomic-design-kit@0';
+export * from 'jsr:@fathym/atomic-design-kit@0.0.193';
